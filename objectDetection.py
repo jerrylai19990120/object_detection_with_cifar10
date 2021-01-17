56484704c8ff60fb11caa0c06b0ff7b65b54ccd1
jerry <jerrylai19990120@gmail.com>
2021-01-17 17:50:46 -0500

    update

--- objectDetection.py
+++ objectDetection.py
@@ -7,25 +7,10 @@ model = tf.keras.models.load_model("detectionModel.h5")
 
 labels = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
 
-cap = cv2.VideoCapture(0)
+img = cv2.imread("DATA/car.jpg")
+resized = cv2.resize(img, (32, 32))
+np_array = np.array(resized)
+reshaped = np.reshape(np_array, (1, 32, 32, 3))
 
-while True:
-
-    ret, frame = cap.read()
-
-    resized = cv2.resize(frame, (32, 32))
-    np_array = np.array(resized)
-    reshaped = np.reshape(np_array, (1, 32, 32, 3))
-
-    res = model.predict(reshaped)
-    print(f"Prediction: {labels[np.argmax(res)]}")
-
-    cv2.imshow("Object Detection", frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-
-
+res = model.predict(reshaped)
+print(f"Prediction: {labels[np.argmax(res)]}")
